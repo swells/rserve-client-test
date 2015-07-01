e01aa3aa5cced9471e42b4e3d44f79add88d5aed
Sean Wells <sean.wells.sc@gmail.com>
2015-07-01 11:59:00 -0700

    first commit

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var Hapi = require('hapi'),
     rclient = require('rserve-client'),
+    ERROR_MSG = 'Invalid code, we do not get real R errors\n.',
     rproxy;
 
 // Create a server with a host and port
@@ -15,15 +16,27 @@ rclient.connect('localhost', 6311, function(err, client) {
 });
 
 // Add the route
-server.route({
-    method: ['GET', 'POST'],
+server.route([{
+    method: 'GET',
     path: '/eval/{code}',
     handler: function(request, reply) {
         rproxy.evaluate(request.params.code, function(err, ans) {
-            reply(err ? 'Invalid code, we do not get real R errors' : ans);
+            reply(err ? ERROR_MSG : ans);
         });
     }
-});
+}, {
+    method: 'POST',
+    path: '/eval',
+    handler: function(request, reply) {
+        if (request.payload) {
+            rproxy.evaluate(request.payload, function(err, ans) {
+                reply(err ? ERROR_MSG : ans);
+            });
+        } else {
+            reply('No HTTP POST content provided.\n');
+        }
+    }
+}]);
 
 // Start the Webserver
 server.start(function() {
